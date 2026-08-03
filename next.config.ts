e39dc8dd864c25451d0b42ee333190b63c608278
phardrecomenda-crypto <phardrecomenda@gmail.com>
2026-08-03 11:12:59 -0300

    feat: launch public commercial homepage

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "beyou-teste-nine.vercel.app" },
+    ],
+  },
 };
 
 export default nextConfig;
